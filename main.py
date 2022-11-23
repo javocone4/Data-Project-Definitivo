435ba426d2dca513cda8d7d27f19948305cb8c7f
Javier <fcojacopu4@gmail.com>
2022-11-23 13:18:53 +0100

    commit 23 nov

--- main.py
+++ main.py
@@ -13,16 +13,21 @@ import psycopg2.extras as extras
   
 #Obtenemos el paquete/caja que nos viene de ahi con el get'
 
-URL= 'https://valencia.opendatasoft.com/explore/dataset/centros-educativos-en-valencia/download/?format=json&timezone=Europe/Berlin&lang=es'
-  
+URL= 'https://valencia.opendatasoft.com/explore/dataset/hospitales/download/?format=json&timezone=Europe/Madrid&lang=es'
+df = pd.read_json(URL)
 #Obtenemos el paquete/caja que nos viene de ahi con el get'
+print(df)
 respuesta = requests.get(url=URL)
 
 datos=respuesta.json()
+
 df = pd.json_normalize(datos)
+df.to_numpy().tolist()
+
+
 #CONEXION A POSTGREESQL
 
-connection = psycopg2.connect(user="postgres", password="Welcome01",host="localhost", port="5432", database="postgres")
+connection = psycopg2.connect(user="postgres", password="Welcome01",host="postgres", port="5432", database="postgres")
 cursor = connection.cursor()
 
 cursor.execute(
@@ -30,15 +35,15 @@ cursor.execute(
     CREATE TABLE IF NOT EXISTS hospitales(
     nombre varchar(50),
     coddistrit integer);
+    
   """
 )
 
 for i in range(len(df)):  
-  print(i)
 
-  postgres_insert_query = """ INSERT INTO hospitales (nombre, coddistrit) VALUES (%s,%s)"""
-
-  record_to_insert = (df['nombre'][i],df['coddistrit'][i])
+  
+  postgres_insert_query = """ INSERT INTO hospitales (nombre,coddistrit) VALUES (%s,%s)"""
+  record_to_insert = (df['fields.barrio'][i],45)
   cursor.execute(postgres_insert_query, record_to_insert)
 
 
